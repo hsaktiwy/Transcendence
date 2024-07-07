f19dcd6f32302c57aef340447a91c094fea2f000
Ahmed Amine Alami <aalami@e3r3p5.1337.ma>
2024-07-07 18:55:22 +0100

    i added a layout for chat session

--- transcendence/src/components/Sidebar.tsx
+++ transcendence/src/components/Sidebar.tsx
@@ -14,8 +14,8 @@ function Sidebar(){
 
     const [active, setActive] = useState<string>('home')
     return(
-        <aside className="bg-[#2B2F32] text-white font-poppins rounded-lg shadow-xl  col-span-1 row-span-12">
-            <div id="logo" className="invisible sm:visible  text-center h-[10%]">
+        <aside className="bg-[#2B2F32] text-white font-poppins rounded-lg shadow-xl  col-span-1 row-span-12 hidden lg:block">
+            {/* <div id="logo" className="invisible sm:visible  text-center h-[10%]">
                 <h1 className="text-[25px] text-white">LOGO</h1>
                 <div className="w-[100%] bg-white h-[2px]"></div>
             </div>
@@ -23,22 +23,18 @@ function Sidebar(){
                 <ul className="h-[100%] flex flex-col justify-center gap-20 lg:mt-8">
                     <li onClick={() => setActive('home')} className={`text-xl pb-4 pt-4 flex justify-center gap-4 items-start cursor-pointer  ${active === 'home' ? 'text-[#ffffff]' : 'text-[#aca8a8]'}`}>
                         <RiHome5Line className="text-[2.5rem]"/>
-                        {/* <span className="hidden lg:inline">Home</span> */}
                     </li>
                     <li onClick={() => setActive('profile')} className={`text-xl pb-4 pt-4 flex justify-center gap-4 items-start cursor-pointer ${active === 'profile' ? 'text-[#ffffff]' : 'text-[#aca8a8]'}`}>
                         <FaRegUser className="text-[2.5rem]"/>
-                        {/* <span className="hidden lg:inline">Profile</span> */}
                     </li>
                     <li onClick={() => setActive('pong')} className={`text-xl pb-4 pt-4 flex justify-center gap-4 items-start cursor-pointer ${active === 'pong' ? 'text-[#ffffff]' : 'text-[#aca8a8]'}`}>
                         <PiPingPongLight className="text-[2.5rem]"/>
-                        {/* <span className="hidden lg:inline">Pong</span> */}
                     </li>
                     <li onClick={() => setActive('logout')} className={`mt-60 text-xl pb-4 pt-4 flex justify-center gap-4 items-start cursor-pointer ${active === 'logout' ? 'text-[#ffffff]' : 'text-[#aca8a8]'}`}>
                         <IoLogOutOutline className="text-[2.5rem]"/>
-                        {/* <span className="hidden lg:inline">Logout</span> */}
                     </li>
                 </ul>
-            </div>
+            </div> */}
 
         </aside>
     )
