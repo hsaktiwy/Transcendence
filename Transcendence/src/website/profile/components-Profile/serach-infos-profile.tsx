64ae40e9adac1a91b634fe77938770b443d4cb42
Hamza Chahboune <hachahbo@e3r9p8.1337.ma>
2024-07-08 15:15:31 +0100

    add env

---
+++ Transcendence/src/website/profile/components-Profile/serach-infos-profile.tsx
@@ -0,0 +1,12 @@
+import './style-component.css'
+import SearchNavBar from './nav-bar-search'
+
+function SearchInfoProfile()
+{
+    return(
+        <div className="search-info-profile">
+            <SearchNavBar/>
+        </div>
+    )
+}
+export default SearchInfoProfile
