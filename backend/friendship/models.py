f70374fd0fd9bc31c131ca01f4e7a6ef687e1838
Hamza Saktiwy <hsaktiwy@e3r2p13.1337.ma>
2024-07-08 19:04:01 +0100

    i did soe initial friendship event they will be changed later to a better onces

--- backend/friendship/models.py
+++ backend/friendship/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from enum import Enum
-from users import User
+from users.models import User
 # Create your models here.
 class RelationShipStatus(Enum):
 	PENDING = 'pending'
@@ -12,12 +12,12 @@ class Friendship(models.Model):
 	friend = models.ForeignKey(User, on_delete=models.CASCADE, related_name='friends')
 	status = models.CharField(max_length=100,
                            choices=[(tag.name, tag.value) for tag in RelationShipStatus],
-                           default=RelationShipStatus.PENDING)
-  	created_at timestamp=now()
+                           default=RelationShipStatus.PENDING.value)
+	created_at = models.DateTimeField(auto_now_add=True)
 
 	class Meta:
-		unique_together = ('user1', 'user2')
-		indexs = [models.Index(fields=['user1', 'user2'],)]
+		unique_together = ('user', 'friend')
+		indexes = [models.Index(fields=['user', 'friend'])]
 
 	def __str__(self):
 		return self.user.login + " - " + self.friend.login + " - " + self.status
