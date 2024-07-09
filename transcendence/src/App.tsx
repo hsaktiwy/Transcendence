bedcf91310d137c802ce2b1e1a1b3e3a1376164d
Ahmed Amine Alami <aalami@e3r9p8.1337.ma>
2024-07-09 17:27:21 +0100

    added some new components

--- transcendence/src/App.tsx
+++ transcendence/src/App.tsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import NavBar from './components/NavBar'
 import Layout from './Layout'
+import ChatSection from './components/ChatSection'
 
 const App = () =>
 {
   return (
       <Layout>
-      
+        <ChatSection/>
       </Layout>
   )
 }
