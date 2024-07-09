1ee0f7ee28d6c3f375830b16e842cd97464d600e
Hamza Saktiwy <hsaktiwy@e3r3p12.1337.ma>
2024-07-09 20:40:17 +0100

    model tournament in progress

--- backend/tournaments/models.py
+++ backend/tournaments/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from game.models import Game
+from users.models import User
 # Create your models here.
+
 class Tournament(models.Model):
   games = models.ManyToManyField(Game)
+  players = models.ManyToManyField(User)
   start  = models.DateTimeField(auto_now_add=True)
   end  = models.DateTimeField(null=True, blank=True)
