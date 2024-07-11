70ea8f84c82cbc4a2a2018a3916b04bf50937e86
Hamza Saktiwy <hsaktiwy@e3r2p14.1337.ma>
2024-07-11 13:56:19 +0100

    create what i can say is a better user model, next step is to create simple user authentication, then make the other models addapte to it specialy the channels app

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -5,3 +5,11 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = MyUser
         fields = ('login', 'firstName', 'lastName', 'email', "birthDay",'password', 'two_factor_auth', 'two_factor_auth_code') 
+    
+    def create(self, validated_data):
+        user = MyUser.objects.create_user(
+            email=validated_data['email'],
+            password=validated_data['password'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', '')
+        )
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,14 +1,14 @@
 #from django.shortcuts import render
 from rest_framework import generics
 from .serializers import UserSerializer
-from .models import User
+from .models import MyUser
 
 
 # Create your views here.
 class UserRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = User.objects.all()
+    queryset = MyUser.objects.all()
     serializer_class = UserSerializer
 
 class UserAPICreate(generics.ListCreateAPIView):
-    queryset = User.objects.all()
+    queryset = MyUser.objects.all()
     serializer_class = UserSerializer
