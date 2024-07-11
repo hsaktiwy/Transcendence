5b66e265f431e4181478f0b050ccc31e337f0f5a
Ahmed Amine Alami <aalami@e3r9p8.1337.ma>
2024-07-11 18:45:26 +0100

    updating some components

--- transcendence/src/components/ChatSession.tsx
+++ transcendence/src/components/ChatSession.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import {ChatSectionContext, ContextType, Conversation, Message} from "../utils/ChatContext"
 import { IoIosMore } from "react-icons/io";
 import { HiPlus } from "react-icons/hi2";
 
@@ -8,45 +9,52 @@ import { RiSendPlaneFill } from "react-icons/ri";
 import Conversations from "./Conversations";
 
 function ChatSession(){
-    interface Message{
-        id: number;
-        sender: string;
-        content: string;
-        profilePicture: string;
-    }
+    const chatContext =useContext(ChatSectionContext)
+    if (!chatContext)
+     throw new Error('error')
+    // interface Message{
+    //     id: number;
+    //     sender: string;
+    //     content: string;
+    //     profilePicture: string;
+    // }
 
-    const messageArray: Message[] = [
-        {id: 2, sender: "Hamza", content: "hey", profilePicture: "./src/assets/8.jpg"},
-        {id: 1, sender: "Amine", content: "hello", profilePicture: "./src/assets/8.jpg"},
-        {id: 2, sender: "Hamza", content: "how are you", profilePicture: "./src/assets/8.jpg"},
-        {id: 1, sender: "Amine", content: "Loreorem ipsum dolor sit amet consectetur adipisicing elit. Unde voluptatibus vel eos non suscipit debitis quaerat laudantium facere eveniet porro? Consectetur consequatur porro repellendus nisi eum laudantium amet numquam accusamus!", profilePicture: "./src/assets/8.jpg"},
-    ]
+    // const messageArray: Message[] = [
+    //     {id: 2, sender: "Hamza", content: "hey", profilePicture: "./src/assets/8.jpg"},
+    //     {id: 1, sender: "Amine", content: "hello", profilePicture: "./src/assets/8.jpg"},
+    //     {id: 2, sender: "Hamza", content: "how are you", profilePicture: "./src/assets/8.jpg"},
+    //     {id: 1, sender: "Amine", content: "Loreorem ipsum dolor sit amet consectetur adipisicing elit. Unde voluptatibus vel eos non suscipit debitis quaerat laudantium facere eveniet porro? Consectetur consequatur porro repellendus nisi eum laudantium amet numquam accusamus!", profilePicture: "./src/assets/8.jpg"},
+    //     {id: 2, sender: "Hamza", content: "Loreorem ipsum dolor sit amet consectetur adipisicing elit. Unde voluptatibus vel eos non suscipit debitis quaerat laudantium facere eveniet porro? Consectetur consequatur porro repellendus nisi eum laudantium amet numquam accusamus!", profilePicture: "./src/assets/8.jpg"},
+    //     {id: 1, sender: "Amine", content: "Loreorem ipsum dolor sit amet consectetur adipisicing elit. Unde voluptatibus vel eos non suscipit debitis quaerat laudantium facere eveniet porro? Consectetur consequatur porro repellendus nisi eum laudantium amet numquam accusamus!", profilePicture: "./src/assets/8.jpg"},
+
+    // ]
+    const messageArray: Message[] = chatContext.active.messages 
     const [backToMessages, setBackToMessages] = useState<boolean>(false)
     return(
 
-        <div className="bg-[#2B2F32] rounded-lg shadow-xl col-span-9 row-span-12 py-2 lg:py-4 px-1 sm:px-4 lg:px-12  font-poppins flex flex-col justify-between overflow-hidden">
+        <div className="bg-[#2B2F32] animate-fade-down rounded-lg shadow-xl col-span-10 lg:col-span-9 row-span-12 py-2 lg:py-4 px-1 sm:px-4 lg:px-12  font-poppins flex flex-col justify-between overflow-hidden">
             <div id="conversation-header-container">
                 <div id="conversation-header" className="text-white grid grid-cols-4 ">
                         <div id="friend-info" className="col-span-2 flex gap-4 lg:gap-8 items-center">
-                            <img src="./src/assets/8.jpg" alt="user-pic" className="h-12 w-12 lg:h-16 lg:w-16 rounded-full" />
+                            <img src={`${chatContext.active.user2.profilePic}`} alt="user-pic" className="w-8 h-8 sm:w-10 sm:h-10 md:h-12 md:w-12 lg:h-16 lg:w-16 rounded-full" />
                             <div >
-                                <p className=" text-md sm:text-lg md:text-xl lg:text-2xl">User User</p>
-                                <p className=" text-sm sm:text-md text-gray-400">@user23424</p>
+                                <p className=" text-sm sm:text-lg md:text-xl lg:text-2xl">{chatContext.active.user2.firstName + " " + chatContext.active.user2.lastName}</p>
+                                <p className=" text-xs sm:text-md text-gray-400">{`@${chatContext.active.user2.username}`}</p>
                             </div>
                         </div>
-                        <span className="col-span-2 justify-self-end self-center text-4xl m-4 cursor-pointer hover:text-[#5E97A9] focus:text-[#5E97A9] duration-300">
+                        <span className="col-span-2 justify-self-end self-center text-sm sm:text-lg md:text-xl lg:text-xl m-4 cursor-pointer hover:text-[#5E97A9] focus:text-[#5E97A9] duration-300">
                         <IoIosMore />
                         </span>
                 </div>
                 <div className="bg-zinc-300 w-[100%] h-[1px] lg:mt-4 rounded-full"></div>
             </div>
-            <div className="text-white basis-[85%]  text-xs sm:text-sm lg:text-lg rounded-lg bg-[#404549] overflow-auto p-5 flex flex-col gap-4">
+            <div className="text-white basis-[85%]  text-xs sm:text-sm lg:text-base rounded-lg bg-[#404549]  p-3 sm:p-5 flex flex-col gap-4 overflow-auto ">
                 {
                     messageArray.map((msg, index): React.ReactNode => {
                         return(
-                        <div key={index} id='message-container' className={` min-h-[140px] flex ${msg.id === 1 && "justify-end flex-row-reverse self-end"} items-end gap-4 `}>
-                            <img src="./src/assets/8.jpg" alt="" className="w-10 h-10 lg:w-12 lg:h-12 rounded-full" />
-                            <div id='message' className={`${msg.id !== 1 ? 'bg-[#5E97A9]' : 'bg-slate-800 '}  p-4 rounded-2xl overflow-hidden`}>
+                        <div key={index} id='message-container' className={`min-h-[180px]  max-h-[187px] flex ${msg.sender.id === 1 && "justify-end flex-row-reverse self-end"} items-end gap-4 `}>
+                            <img src={`${msg.sender.profilePic}`} alt="" className="hidden sm:block w-8 h-8 sm:w-10 sm:h-10 lg:w-12 lg:h-12 rounded-full" />
+                            <div id='message' className={`${msg.sender.id !== 1 ? 'bg-[#5E97A9]' : 'bg-slate-800 '}  p-4 rounded-2xl max-h-[100%] overflow-y-auto overflow-x-hidden`}>
                                 <p >{msg.content}</p>
                             </div>
                         </div>
--- transcendence/src/components/Conversations.tsx
+++ transcendence/src/components/Conversations.tsx
@@ -1,125 +1,142 @@
-import React from "react";
+// import React from "react";
 
-function Conversations(){
+// function Conversations(){
+
+//     interface Message{
+//         id: number;
+//         sender: string;
+//         content: string;
+//         profilePicture: string;
+//     }
+//     interface Conversations {
+//         channelId: number;
+//         messages: Message[];
+//     }
+
+//     const convs: Conversations[] = [
+
+//         {
+//             channelId: 1, 
+//             messages: [
+//                 {id: 2, sender: "Hamza", content: "hey", profilePicture: "./src/assets/8.jpg"},
+//                 {id: 1, sender: "Amine", content: "hello", profilePicture: "./src/assets/8.jpg"},
+//                 {id: 2, sender: "Hamza", content: "how are you", profilePicture: "./src/assets/8.jpg"},
+//                 {id: 1, sender: "Amine", content: "Loreorem ipsum dolor sit amet consectetur adipisicing elit. Unde voluptatibus vel eos non suscipit debitis quaerat laudantium facere eveniet porro? Consectetur consequatur porro repellendus nisi eum laudantium amet numquam accusamus!", profilePicture: "./src/assets/8.jpg"},
+//                 {id: 2, sender: "Hamza", content: "Loreorem ipsum dolor sit amet consectetur adipisicing elit. Unde voluptatibus vel eos non suscipit debitis quaerat laudantium facere eveniet porro? Consectetur consequatur porro repellendus nisi eum laudantium amet numquam accusamus!", profilePicture: "./src/assets/8.jpg"},
+//                 {id: 1, sender: "Amine", content: "Loreorem ipsum dolor sit amet consectetur adipisicing elit. Unde voluptatibus vel eos non suscipit debitis quaerat laudantium facere eveniet porro? Consectetur consequatur porro repellendus nisi eum laudantium amet numquam accusamus!", profilePicture: "./src/assets/8.jpg"},
+//             ]
+//         },
+//         {
+//             channelId: 2, 
+//             messages: [
+//                 {id: 2, sender: "Hamza", content: "hey", profilePicture: "./src/assets/8.jpg"},
+//                 {id: 1, sender: "Amine", content: "hello", profilePicture: "./src/assets/8.jpg"},
+//             ]
+//         },
+//         {
+//             channelId: 3, 
+//             messages: [
+//                 {id: 1, sender: "Amine", content: "hello", profilePicture: "./src/assets/8.jpg"},
+//                 {id: 2, sender: "Hamza", content: "hey", profilePicture: "./src/assets/8.jpg"},
+//             ]
+//         }
+//     ]
    
+// return(
+//     <div className="bg-[#323232] rounded-lg shadow-xl col-span-3 row-span-12 px-0 sm:px-2 pt-4 font-poppins lg:flex lg:flex-col overflow-auto">
+//             <div className="messages-header-container hidden lg:text-xl text-white lg:flex flex-col items-center gap-8">
+//                 <h1>Messages</h1>
+//                 <div className="bg-zinc-300 w-[100%] h-[1px] mt-4 rounded-full"></div>
+//             </div>
+//             <div id="messages-conatiner" className="text-white my-2 flex flex-col gap-2 ">
+//                 <div className=" mb-4 flex justify-center xl:justify-start gap-6">
+
+//                     <img src="./src/assets/8.jpg" alt="friend-pic" className="rounded-full  object-contain w-12 h-12  lg:w-10 lg:h-10 2xl:w-16 2xl:h-16" />
+//                     <div className="self-center hidden lg:block">
+//                         <h1 className="text-sm xl:text-md 2xl:text-lg">Lorem ipsum</h1>
+//                         <p className="text-gray-300 text-xs lg:text-md">Lorem ipsum dolor sit amet...</p>
+//                     </div>
+//                 </div>
+//                 <div className=" mb-4 flex justify-center xl:justify-start gap-6">
+
+//                     <img src="./src/assets/8.jpg" alt="friend-pic" className="rounded-full  object-contain w-12 h-12  lg:w-10 lg:h-10 2xl:w-16 2xl:h-16" />
+//                     <div className="self-center hidden lg:block">
+//                         <h1 className="text-sm xl:text-md 2xl:text-lg">Lorem ipsum</h1>
+//                         <p className="text-gray-300 text-xs lg:text-md">Lorem ipsum dolor sit amet...</p>
+//                     </div>
+//                 </div>
+//                 <div className=" mb-4 flex justify-center xl:justify-start gap-6">
+
+//                     <img src="./src/assets/8.jpg" alt="friend-pic" className="rounded-full  object-contain w-12 h-12  lg:w-10 lg:h-10 2xl:w-16 2xl:h-16" />
+//                     <div className="self-center hidden lg:block">
+//                         <h1 className="text-sm xl:text-md 2xl:text-lg">Lorem ipsum</h1>
+//                         <p className="text-gray-300 text-xs lg:text-md">Lorem ipsum dolor sit amet...</p>
+//                     </div>
+//                 </div>
+//                 <div className=" mb-4 flex justify-center xl:justify-start gap-6">
+
+//                     <img src="./src/assets/8.jpg" alt="friend-pic" className="rounded-full  object-contain w-12 h-12  lg:w-10 lg:h-10 2xl:w-16 2xl:h-16" />
+//                     <div className="self-center hidden lg:block">
+//                         <h1 className="text-sm xl:text-md 2xl:text-lg">Lorem ipsum</h1>
+//                         <p className="text-gray-300 text-xs lg:text-md">Lorem ipsum dolor sit amet...</p>
+//                     </div>
+//                 </div>
+//             </div>
+//     </div>
+// )
+
+// }
+
+// export default Conversations
+import React, { useContext, useState } from "react";
+import {ChatSectionContext, ContextType, Conversation, Message} from "../utils/ChatContext"
+function Conversations(){
+   const chatContext =useContext(ChatSectionContext)
+   if (!chatContext)
+    throw new Error('error')
+  
 return(
-    <div className="bg-[#323232] rounded-lg shadow-xl col-span-3 row-span-12 px-0 sm:px-2 pt-4 font-poppins lg:flex lg:flex-col overflow-auto">
+    <div className="bg-[#323232] rounded-lg shadow-xl col-span-2 lg:col-span-3 row-span-12 px-0 sm:px-2 pt-4 font-poppins lg:flex lg:flex-col overflow-auto">
             <div className="messages-header-container hidden lg:text-xl text-white lg:flex flex-col items-center gap-8">
                 <h1>Messages</h1>
                 <div className="bg-zinc-300 w-[100%] h-[1px] mt-4 rounded-full"></div>
             </div>
             <div id="messages-conatiner" className="text-white my-2 flex flex-col gap-2 ">
-                <div className=" mb-4 flex justify-center xl:justify-start gap-6">
-                        <img src="./src/assets/8.jpg" alt="friend-pic" className="rounded-full  object-contain w-12 h-12  lg:w-10 lg:h-10 2xl:w-16 2xl:h-16" />
-                        <div className="self-center hidden lg:block">
-                            <h1 className="text-sm xl:text-md 2xl:text-lg">Lorem ipsum</h1>
-                            <p className="text-gray-300 text-xs lg:text-md">Lorem ipsum dolor sit amet...</p>
-                        </div>
-                </div>
-                <div className=" mb-4 flex justify-center xl:justify-start gap-6">
+                {/* <div className=" mb-4 flex justify-center xl:justify-start gap-6"> */}
+                    {
+                        chatContext.convs.map((conv, index): React.ReactNode => {
+                            interface convData{
+                                lastMessage: string;
+                                picture: string;
+                                friendName: string;
+                            }
+                            const currentConvData: convData = {
+                                lastMessage: conv.messages[conv.messages.length - 1].content,
+                                picture: conv.user2.profilePic,
+                                friendName: conv.user2.firstName +  " " +conv.user2.lastName
+                            }
+                            return(
+                                
+                                <div key={index} className={` ${conv.channelId === chatContext.active.channelId ? "bg-black" : ""} mb-4 flex justify-center lg:justify-start gap-6 cursor-pointer hover:bg-black duration-150 rounded-2xl p-4`} onClick={() =>{
+                                    chatContext.setActive(conv)
+                                }}>
+                                        <img src={currentConvData.picture} alt="friend-pic" className="rounded-full  object-contain w-12 h-12  lg:w-10 lg:h-10 2xl:w-16 2xl:h-16" />
+                                        <div className="self-center hidden lg:block">
+                                            <h1 className="text-sm xl:text-md 2xl:text-lg">{currentConvData.friendName}</h1>
+                                            <p className="text-gray-300 text-xs lg:text-md">{currentConvData.lastMessage.length >= 50 ? currentConvData.lastMessage.substring(0,50) + "..." : currentConvData.lastMessage}</p>
+                                        </div>
+                                    </div>
+                               
 
-                    <img src="./src/assets/8.jpg" alt="friend-pic" className="rounded-full  object-contain w-12 h-12  lg:w-10 lg:h-10 2xl:w-16 2xl:h-16" />
+                            )
+                        })
+                    }
+                    {/* <img src="./src/assets/8.jpg" alt="friend-pic" className="rounded-full  object-contain w-12 h-12  lg:w-10 lg:h-10 2xl:w-16 2xl:h-16" />
                     <div className="self-center hidden lg:block">
                         <h1 className="text-sm xl:text-md 2xl:text-lg">Lorem ipsum</h1>
                         <p className="text-gray-300 text-xs lg:text-md">Lorem ipsum dolor sit amet...</p>
-                    </div>
-                </div>
-                <div className=" mb-4 flex justify-center xl:justify-start gap-6">
-
-                    <img src="./src/assets/8.jpg" alt="friend-pic" className="rounded-full  object-contain w-12 h-12  lg:w-10 lg:h-10 2xl:w-16 2xl:h-16" />
-                    <div className="self-center hidden lg:block">
-                        <h1 className="text-sm xl:text-md 2xl:text-lg">Lorem ipsum</h1>
-                        <p className="text-gray-300 text-xs lg:text-md">Lorem ipsum dolor sit amet...</p>
-                    </div>
-                </div>
-                <div className=" mb-4 flex justify-center xl:justify-start gap-6">
-
-                    <img src="./src/assets/8.jpg" alt="friend-pic" className="rounded-full  object-contain w-12 h-12  lg:w-10 lg:h-10 2xl:w-16 2xl:h-16" />
-                    <div className="self-center hidden lg:block">
-                        <h1 className="text-sm xl:text-md 2xl:text-lg">Lorem ipsum</h1>
-                        <p className="text-gray-300 text-xs lg:text-md">Lorem ipsum dolor sit amet...</p>
-                    </div>
-                </div>
-                <div className=" mb-4 flex justify-center xl:justify-start gap-6">
-
-                    <img src="./src/assets/8.jpg" alt="friend-pic" className="rounded-full  object-contain w-12 h-12  lg:w-10 lg:h-10 2xl:w-16 2xl:h-16" />
-                    <div className="self-center hidden lg:block">
-                        <h1 className="text-sm xl:text-md 2xl:text-lg">Lorem ipsum</h1>
-                        <p className="text-gray-300 text-xs lg:text-md">Lorem ipsum dolor sit amet...</p>
-                    </div>
-                </div>
-                <div className=" mb-4 flex justify-center xl:justify-start gap-6">
-
-                    <img src="./src/assets/8.jpg" alt="friend-pic" className="rounded-full  object-contain w-12 h-12  lg:w-10 lg:h-10 2xl:w-16 2xl:h-16" />
-                    <div className="self-center hidden lg:block">
-                        <h1 className="text-sm xl:text-md 2xl:text-lg">Lorem ipsum</h1>
-                        <p className="text-gray-300 text-xs lg:text-md">Lorem ipsum dolor sit amet...</p>
-                    </div>
-                </div>
-                <div className=" mb-4 flex justify-center xl:justify-start gap-6">
-
-                    <img src="./src/assets/8.jpg" alt="friend-pic" className="rounded-full  object-contain w-12 h-12  lg:w-10 lg:h-10 2xl:w-16 2xl:h-16" />
-                    <div className="self-center hidden lg:block">
-                        <h1 className="text-sm xl:text-md 2xl:text-lg">Lorem ipsum</h1>
-                        <p className="text-gray-300 text-xs lg:text-md">Lorem ipsum dolor sit amet...</p>
-                    </div>
-                </div>
-                <div className=" mb-4 flex justify-center xl:justify-start gap-6">
-
-                    <img src="./src/assets/8.jpg" alt="friend-pic" className="rounded-full  object-contain w-12 h-12  lg:w-10 lg:h-10 2xl:w-16 2xl:h-16" />
-                    <div className="self-center hidden lg:block">
-                        <h1 className="text-sm xl:text-md 2xl:text-lg">Lorem ipsum</h1>
-                        <p className="text-gray-300 text-xs lg:text-md">Lorem ipsum dolor sit amet...</p>
-                    </div>
-                </div>
-                <div className=" mb-4 flex justify-center xl:justify-start gap-6">
-
-                    <img src="./src/assets/8.jpg" alt="friend-pic" className="rounded-full  object-contain w-12 h-12  lg:w-10 lg:h-10 2xl:w-16 2xl:h-16" />
-                    <div className="self-center hidden lg:block">
-                        <h1 className="text-sm xl:text-md 2xl:text-lg">Lorem ipsum</h1>
-                        <p className="text-gray-300 text-xs lg:text-md">Lorem ipsum dolor sit amet...</p>
-                    </div>
-                </div>
-                <div className=" mb-4 flex justify-center xl:justify-start gap-6">
-
-                    <img src="./src/assets/8.jpg" alt="friend-pic" className="rounded-full  object-contain w-12 h-12  lg:w-10 lg:h-10 2xl:w-16 2xl:h-16" />
-                    <div className="self-center hidden lg:block">
-                        <h1 className="text-sm xl:text-md 2xl:text-lg">Lorem ipsum</h1>
-                        <p className="text-gray-300 text-xs lg:text-md">Lorem ipsum dolor sit amet...</p>
-                    </div>
-                </div>
-                <div className=" mb-4 flex justify-center xl:justify-start gap-6">
-
-                    <img src="./src/assets/8.jpg" alt="friend-pic" className="rounded-full  object-contain w-12 h-12  lg:w-10 lg:h-10 2xl:w-16 2xl:h-16" />
-                    <div className="self-center hidden lg:block">
-                        <h1 className="text-sm xl:text-md 2xl:text-lg">Lorem ipsum</h1>
-                        <p className="text-gray-300 text-xs lg:text-md">Lorem ipsum dolor sit amet...</p>
-                    </div>
-                </div>
-                <div className=" mb-4 flex justify-center xl:justify-start gap-6">
-
-                    <img src="./src/assets/8.jpg" alt="friend-pic" className="rounded-full  object-contain w-12 h-12  lg:w-10 lg:h-10 2xl:w-16 2xl:h-16" />
-                    <div className="self-center hidden lg:block">
-                        <h1 className="text-sm xl:text-md 2xl:text-lg">Lorem ipsum</h1>
-                        <p className="text-gray-300 text-xs lg:text-md">Lorem ipsum dolor sit amet...</p>
-                    </div>
-                </div>
-                <div className=" mb-4 flex justify-center xl:justify-start gap-6">
-
-                    <img src="./src/assets/8.jpg" alt="friend-pic" className="rounded-full  object-contain w-12 h-12  lg:w-10 lg:h-10 2xl:w-16 2xl:h-16" />
-                    <div className="self-center hidden lg:block">
-                        <h1 className="text-sm xl:text-md 2xl:text-lg">Lorem ipsum</h1>
-                        <p className="text-gray-300 text-xs lg:text-md">Lorem ipsum dolor sit amet...</p>
-                    </div>
-                </div>
-                <div className=" mb-4 flex justify-center xl:justify-start gap-6">
-
-                    <img src="./src/assets/8.jpg" alt="friend-pic" className="rounded-full  object-contain w-12 h-12  lg:w-10 lg:h-10 2xl:w-16 2xl:h-16" />
-                    <div className="self-center hidden lg:block">
-                        <h1 className="text-sm xl:text-md 2xl:text-lg">Lorem ipsum</h1>
-                        <p className="text-gray-300 text-xs lg:text-md">Lorem ipsum dolor sit amet...</p>
-                    </div>
-                </div>
+                    </div> */}
+                {/* </div> */}
             </div>
     </div>
 )
