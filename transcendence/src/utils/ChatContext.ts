a51453a305e7149232d49bccb56ab2ca9b7a1350
Ahmed Amine Alami <aalami@e3r9p8.1337.ma>
2024-07-11 18:45:26 +0100

    updating some components

---
+++ transcendence/src/utils/ChatContext.ts
@@ -0,0 +1,29 @@
+import { createContext } from "react";
+
+export interface User{
+    id: number;
+    firstName: string;
+    lastName: string;
+    username: string;
+    profilePic: string;
+}
+export interface Message{
+    id: number;
+    sender: User;
+    content: string;
+}
+export interface Conversation {
+    channelId: number;
+    user1: User;
+    user2: User;
+    messages: Message[];
+}
+export interface ContextType {
+    convs: Conversation[];
+    setActive: React.Dispatch<React.SetStateAction<Conversation>>;
+    active: Conversation
+    activeSectionOnSm: React.ReactNode 
+    setActiveSection: React.Dispatch<React.SetStateAction<React.ReactNode >>;
+}
+
+export const ChatSectionContext = createContext<ContextType | undefined>(undefined)
