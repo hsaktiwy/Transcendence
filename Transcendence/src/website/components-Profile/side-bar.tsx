f19e33d880e48c2d94db9f5aec2b0e942a36c86b
Hamza Chahboune <hachahbo@e3r8p18.1337.ma>
2024-07-16 06:48:05 +0100

    add test component

--- Transcendence/src/website/components-Profile/side-bar.tsx
+++ Transcendence/src/website/components-Profile/side-bar.tsx
@@ -4,31 +4,34 @@ import './style-component.css'
 function sideBar()
 {
     return(
-        <div className="side-bar-div lineRight">
-            <div className="side-bar-div-grid logo-center">
-                <img src="/images/Logo.svg" />
+        <div className="border w-1/5 max-w-64 min-w-[200px]">
+            <div className="side-bar-div lineRight">
+                <div className="side-bar-div-grid  logo-center">
+                    <img className="" src="/images/Logo (1).svg" />
+                </div>
+                <div className="side-bar-div-grid center-sid-bar">
+                    <div className="div-side-bar-icon">
+                        <img className="w-9 md:size-image-bar " src="/images/Home.svg" />
+                        <h1 style={{fontWeight: '400'}} className="hidden md:block pl-2" >Home</h1>
+                    </div>
+                    <div className="div-side-bar-icon">
+                        <img className="w-9 md:size-image-bar "  src="/images/profileVector.svg" />
+                        <h1 style={{fontWeight: '400'}} className="hidden md:block pl-2">Profile</h1>
+                    </div>
+                    <div className="div-side-bar-icon">
+                        <img className="w-9 md:size-image-bar " src="/images/Message.svg" />
+                        <h1 style={{fontWeight: '400'}} className="hidden md:block pl-2">Chatt</h1>
+                    </div>
+                    <div className="div-side-bar-icon">
+                        <img className="w-9 md:size-image-bar " src="/images/pong_.svg" />
+                        <h1 style={{fontWeight: '400'}} className="hidden md:block pl-2">Game</h1>
+                    </div>
+                </div>
+                <div className="side-bar-div-grid log-out">
+                    <img src="/images/log-out.svg" />
+                </div>
             </div>
-            <div className="side-bar-div-grid center-sid-bar">
-                <div className="div-side-bar-icon">
-                    <img className="size-image-bar" src="/images/Home.svg" />
-                    <h1 style={{fontWeight: '400'}} >Home</h1>
-                </div>
-                <div className="div-side-bar-icon">
-                    <img className="size-image-bar" src="/images/profileVector.svg" />
-                    <h1 style={{fontWeight: '400'}} >Profile</h1>
-                </div>
-                <div className="div-side-bar-icon">
-                    <img className="size-image-bar" src="/images/Message.svg" />
-                    <h1 style={{fontWeight: '400'}} >Chatt</h1>
-                </div>
-                <div className="div-side-bar-icon">
-                    <img className="size-image-bar" src="/images/pong_.svg" />
-                    <h1 style={{fontWeight: '400'}} >Game</h1>
-                </div>
-            </div>
-            <div className="side-bar-div-grid log-out">
-                <img src="/images/log-out.svg" />
-            </div>
+
         </div>
     )
 }
