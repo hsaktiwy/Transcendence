5c92f4ca795b02fef14491e0a7b0ca0ead99b551
Hamza Chahboune <hachahbo@e3r9p16.1337.ma>
2024-07-17 06:58:06 +0100

    creat side bar and dashbord part

--- Transcendence/src/website/components-Profile/testcmpo.tsx
+++ Transcendence/src/website/components-Profile/testcmpo.tsx
@@ -30,15 +30,14 @@ function testcmpo()
                     </div> */}
         
             <div className="grid gap-8 justify-center text-center sm:grid-cols-2 sm:items-center sm:text-left">
-
                     <img className="mx-auto rounded-lg " src="../../../public/images/musashi-miyamoto-from-vagabond-v0-o83baybb71oc1.png"/> 
                     <div >
-                    <h1 className="text-4xl font-medium ">Musachi  Miyamoto</h1>
-                    <p>dustry. Lorem Ipsum has been the industry's standard dummy
-                    text ever since the 1500s, when an unknown printer took
-                    a galley of type and scrambled it to make a type specimen book.
-                    It has survived not only five centuries, but also the leap into 
-                    versions of Lorem Ipsum</p>
+                        <h1 className="text-4xl font-medium ">Musachi  Miyamoto</h1>
+                        <p>dustry. Lorem Ipsum has been the industry's standard dummy
+                        text ever since the 1500s, when an unknown printer took
+                        a galley of type and scrambled it to make a type specimen book.
+                        It has survived not only five centuries, but also the leap into 
+                        versions of Lorem Ipsum</p>
                 </div>
             </div>
         </>
