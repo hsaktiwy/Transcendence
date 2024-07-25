7ffd1d8710016e2bf2cbff85ce97a3f8c982ea02
L7EMZA <l7emza@bocals-iMac-2.local>
2024-07-25 15:40:49 +0100

    profile section

--- Transcendence/src/website/components-Profile/side-bar.tsx
+++ Transcendence/src/website/components-Profile/side-bar.tsx
@@ -12,19 +12,15 @@ function sideBar()
                 <div className="side-bar-div-grid center-sid-bar">
                     <div className="div-side-bar-icon">
                         <img className="w-9 md:size-image-bar " src="/images/Home.svg" />
-                        <h1 style={{fontWeight: '400'}} className="hidden md:block pl-2" >Home</h1>
                     </div>
-                    <div className="div-side-bar-icon">
+                    {/* <div className="div-side-bar-icon">
                         <img className="w-9 md:size-image-bar "  src="/images/profileVector.svg" />
-                        <h1 style={{fontWeight: '400'}} className="hidden md:block pl-2">Profile</h1>
-                    </div>
+                    </div> */}
                     <div className="div-side-bar-icon">
                         <img className="w-9 md:size-image-bar " src="/images/Message.svg" />
-                        <h1 style={{fontWeight: '400'}} className="hidden md:block pl-2">Chatt</h1>
                     </div>
                     <div className="div-side-bar-icon">
                         <img className="w-9 md:size-image-bar " src="/images/pong_.svg" />
-                        <h1 style={{fontWeight: '400'}} className="hidden md:block pl-2">Game</h1>
                     </div>
                 </div>
                 <div className="side-bar-div-grid log-out">
