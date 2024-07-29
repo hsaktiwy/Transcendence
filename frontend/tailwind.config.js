f03ed901f5ed2607dc31bed4173889ab7bc40353
Hamza Saktiwy <hsaktiwy@e3r4p11.1337.ma>
2024-07-29 03:22:56 +0100

    fixed the problem in the update message, every thing work fine

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -12,6 +12,20 @@ export default {
         'xsm': '421px' ,
       }
     },
+    keyframes: {
+      wiggle: {
+        '0%, 100%': { transform: 'rotate(-3deg)' },
+        '50%': { transform: 'rotate(3deg)' },
+      },
+      fadeIn: {
+        '0%': { opacity: 0 },
+        '100%': { opacity: 1 },
+      },
+    },
+    animation: {
+      wiggle: 'wiggle 1s ease-in-out infinite',
+      fadeIn: 'fadeIn 1s ease-in-out',
+    },
   },
   plugins: [
     require('tailwindcss-animated')
