aafffceebb10d1ae55a634b952e56183e40480e5
Hamza Saktiwy <hsaktiwy@e3r9p16.1337.ma>
2024-08-07 13:18:28 +0100

    go to y pc hahahah

---
+++ frontend/src/game/Game.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import LoadingIndecator from "../components/Loading";
+
+function Game()
+{
+    const [send, setSend] = useState<boolean>(false);
+
+    useEffect(()=>{
+        console.log('rendring : '+( send ? 'True' :  'False'))
+    },[send])
+
+    const FoundOpponent = () =>
+    {
+        // we will send a ws request to the server to found a Opponent
+        if (!send)
+        {
+            console.log('Send Request')
+            setSend(true);
+        }
+    }
+
+    return (
+        <div className="flex justify-center items-center h-full">
+            
+            { send ? <LoadingIndecator/> : <input type="button" value="FoundOpponent" className=" h-fit px-6 py-3 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75 active:bg-blue-800 transition duration-150 ease-in-out"
+                onClick={FoundOpponent}
+            />}
+        </div>
+    )
+}
+
+export default Game;
--- frontend/src/utils/Constants.ts
+++ frontend/src/utils/Constants.ts
@@ -1,4 +1,4 @@
-export const BACKEND:string = "http://10.13.8.16:8000/"
+export const BACKEND:string = "http://10.13.9.15:8000/"
 export const LOGIN_PATH:string = 'login/'
 export const SESSION_CHECKPATH:string = 'api/session/'
 export const INIT_CSRFTOKEN_PATH:string = "csrftoken/"
@@ -6,7 +6,7 @@ export const ADD_USER:string = "api/user/create/"
 export const CONVERSATION:string = "chat/conversations/" 
 export const MULTIFORMDATA_BOUNDARY_HASH:number =  20 // this will be the random string size (for more info search for Multi Format Data related to POST methode), that will be added to our boundary
 export const ws_scheame = 'ws'
-export const ws_host = '10.13.8.16'
+export const ws_host = '10.13.9.15'
 export const ws_port = '8000'
 export const ws_url = ws_scheame + '://' + ws_host + ':' + ws_port;
 export let user_id:number;
