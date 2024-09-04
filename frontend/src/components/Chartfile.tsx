2ea73f949e0d6f53107e8e90f8aa01a4316f9f61
Hamza Chahboune <hachahbo@e3r4p8.1337.ma>
2024-09-04 15:01:53 +0100

    finishing the dashboard front

--- frontend/src/components/Chartfile.tsx
+++ frontend/src/components/Chartfile.tsx
@@ -1,17 +1,18 @@
 import { Button } from "@/components/ui/button"
 
- import { Bar, BarChart } from "recharts"
+ import { Bar, BarChart,XAxis } from "recharts"
+
  
 import { ChartConfig, ChartContainer } from "@/components/ui/chart"
  
 const chartData = [
-  { month: "January", desktop: 186, mobile: 80 },
-  { month: "February", desktop: 305, mobile: 200 },
-  { month: "March", desktop: 237, mobile: 120 },
-  { month: "April", desktop: 73, mobile: 190 },
-  { month: "May", desktop: 209, mobile: 130 },
-  { month: "June", desktop: 114, mobile: 140 },
-  { month: "June", desktop: 90, mobile: 140 },
+  { week: "Monday", desktop: 186, mobile: 80 },
+  { week: "Tuesday", desktop: 305, mobile: 200 },
+  { week: "Wednesday", desktop: 237, mobile: 120 },
+  { week: "Thursday", desktop: 73, mobile: 190 },
+  { week: "Friday", desktop: 209, mobile: 130 },
+  { week: "Saturday", desktop: 114, mobile: 140 },
+  { week: "Sunday", desktop: 90, mobile: 140 },
 ]
  
 const chartConfig = {
@@ -31,6 +32,14 @@ export function ChartFile() {
     <ChartContainer config={chartConfig} className="min-h-[200px] w-full">
       <BarChart accessibilityLayer data={chartData}>
         <Bar dataKey="desktop" fill="var(--color-desktop)" radius={7} />
+        <XAxis
+              dataKey="week"
+              tickLine={false}
+              axisLine={false}
+              tickMargin={8}
+              tick={{ fill: 'white' }} // Change the color here
+              tickFormatter={(value) => value.slice(0, 3)}
+            />
       </BarChart>
     </ChartContainer>
   </>
