20753bb5b49ddb165d88f55e0f364a2969f1d4bc
Ahmed Amine Alami <aalami@e1r4p4.1337.ma>
2024-10-11 23:53:17 +0100

    starting on run the project with docker it works till now :)

--- frontend/src/components/SecuritySettings.tsx
+++ frontend/src/components/SecuritySettings.tsx
@@ -2,16 +2,18 @@ import React, { useState } from "react";
 import { FiEdit2 } from "react-icons/fi";
 import { LuEye } from "react-icons/lu";
 import { LuEyeOff } from "react-icons/lu";
+import { GoQuestion } from "react-icons/go";
+
 
 function SecuritySettings(){
-    const [hide, setHide] = useState<boolean[]>([true, true])
+    const [hide, setHide] = useState<boolean[]>([true, true, true])
     const togglePassword = () =>{
         
     }
     return(
         <div className=" animate-fadeIn security-settings m-10 flex-1 bg-gradient-to-b from-slate-300/10 to-cyan-500/10 rounded-xl flex flex-col gap-10">
             <div className=" px-10 py-4 flex justify-between ">
-                <h1 className="text-2xl font-semibold">Change Password</h1>
+                <h1 className="text-3xl ">Change Password</h1>
                 <form action="" className="flex flex-col gap-8 ">
                     <div className="relative w-[700px]">
                         <label htmlFor="oldPassword"></label>
@@ -26,10 +28,10 @@ function SecuritySettings(){
                             {hide[0] ? <LuEyeOff/> : <LuEye/>}
                         </div>
                     </div>
-                    <div className="relative w-[700px]">
+                    <div className="relative w-[700px] flex flex-col gap-4">
                         <label htmlFor="newPassword"></label>
                         <input type={hide[1]  ? 'password' : 'text'} placeholder="New Password" className="bg-black/35 backdrop-filter backdrop-blur-sm rounded-2xl py-2 px-4  w-full outline-none focus:outline-1 focus:outline-[#5E97A9]"/>
-                        <div className=" p-4 text-lg duration-100  absolute right-1 text-white top-[50%] -translate-y-[50%] cursor-pointer " onClick={() =>{
+                        <div className=" p-4 text-lg duration-100  absolute right-1 text-white top-[40%] -translate-y-[50%] cursor-pointer " onClick={() =>{
                             setHide(prev => {
                                 const newArr = [...prev]
                                 newArr[1] = !newArr[1]
@@ -37,6 +39,23 @@ function SecuritySettings(){
                             })
                         }}>
                             {hide[1] ? <LuEyeOff/> : <LuEye/>}
+                        </div>
+                        <div className="flex justify-start items-center text-white/70 opacity-70 gap-5 px-4">
+                            <span className="inline-block  text-xl"><GoQuestion/></span>
+                            <p className=" ">Your Password must be different to previously used password</p>
+                        </div>
+                    </div>
+                    <div className="relative w-[700px]">
+                        <label htmlFor="confirmNewPassword"></label>
+                        <input type={hide[2]  ? 'password' : 'text'} placeholder="Confirm New Password" className="bg-black/35 backdrop-filter backdrop-blur-sm rounded-2xl py-2 px-4  w-full outline-none focus:outline-1 focus:outline-[#5E97A9]"/>
+                        <div className=" p-4 text-lg duration-100  absolute right-1 text-white top-[50%] -translate-y-[50%] cursor-pointer " onClick={() =>{
+                            setHide(prev => {
+                                const newArr = [...prev]
+                                newArr[2] = !newArr[2]
+                                return newArr
+                            })
+                        }}>
+                            {hide[2] ? <LuEyeOff/> : <LuEye/>}
                         </div>
                     </div>
 
@@ -48,16 +67,8 @@ function SecuritySettings(){
                     </div>
                 </form>
             </div>
-            <div className="px-10 py-4 flex justify-between">
-                <div className="flex flex-col gap-4 ">
-                    <h1 className="text-2xl font-semibold">Two Factory Authentication</h1>
-                    <p className="text-white/70 text-lg">Enable two factory authentication for a secure experience</p>
-                    <p className="px-6">1- Scan the QR CODE using Google Authenticator</p>
-                    <div className="h-[500px] w-[500px]">
-                        <img src="qr.png" alt="qr-code" className="w-full h-full object-cover"/>
-                    </div>
-                    <p className="px-6">2- Enter the Code</p>
-                </div>
+            <div className=" px-10 py-4 flex justify-between ">
+                <h1 className="text-3xl ">Two-Factor Authentication</h1>
             </div>
         </div>
     )
