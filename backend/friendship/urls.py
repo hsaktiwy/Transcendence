9fab5a1c15abd21f3c52c0b1f59275911fcddadc
hsaktiwy <hsaktiwy@e1r6p9.1337.ma>
2024-11-01 18:09:45 +0100

    added the functionality of unfrending people

--- backend/friendship/urls.py
+++ backend/friendship/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import FriendRequestList, AcceptFriendRequest, BlockUser
+from .views import FriendRequestList, AcceptFriendRequest, BlockUser, UnFriendUser
 urlpatterns = [
    path("requests/", FriendRequestList.as_view(), name='listfriendRequest'),
    path("request/status/set/accept/<int:id>", AcceptFriendRequest, name="FirendRequest"),
-   path("block/<str:_login>", BlockUser ,name="BlockUser")
+   path("block/<str:_login>", BlockUser ,name="BlockUser"),
+   path("unfriend/<str:_login>", UnFriendUser ,name="Unfriend")
 ]
