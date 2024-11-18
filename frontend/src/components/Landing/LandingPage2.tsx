a8d57fc0771a475443f21503593c1f9e2731cf83
Ahmed Amine Alami <aalami@e2r8p13.1337.ma>
2024-11-18 20:33:17 +0100

    starting on a new landing page

--- frontend/src/components/Landing/LandingPage2.tsx
+++ frontend/src/components/Landing/LandingPage2.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import NavBar from "./NavBar";
+import HeroSection from "./HeroSection";
+import CardBorder from "../../../public/cardBorder.png"
+import TargetImage from "../../../public/target-front-color.png"
 const LandingPage2 = () =>{
 
     return(
         <main className="bg-gradient-to-b from-[#070320] to-[#1a3340] font-poppins min-h-[100vh]">
-            <section className="py-10 mx-auto container ">
+            <section className="p-10 mx-auto container text-white flex flex-col gap-16 relative h-[100vh]">
                 <NavBar/>
+                <HeroSection/>
             </section>
+
             
         </main>
     )
