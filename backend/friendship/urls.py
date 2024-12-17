96aafa6175ce5e766a0acbfa0bf5a78206d495cd
hsaktiwy <hsaktiwy@gmail.com>
2024-12-17 22:47:31 +0100

    beta version : of the friendship handling in the profile is uploaded, feal free to judge the ligic and sudjets updates

--- backend/friendship/urls.py
+++ backend/friendship/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import FriendRequestList, AcceptFriendRequest, BlockUser, UnFriendUser, isBlocked, UnBlockUser,FriendRequestSentList, FriendRequestReceivedList, isFriend, FriendRequestStatus
+from .views import FriendRequestList, AcceptFriendRequest, BlockUser, UnFriendUser, isBlocked, UnBlockUser,FriendRequestSentList, FriendRequestReceivedList, isFriend, FriendRequestStatus, CancelFriendRequest
 urlpatterns = [
    path("requests/", FriendRequestList.as_view(), name='listfriendRequest'),
    path("request/status/set/accept/<int:id>", AcceptFriendRequest, name="FirendRequest"),
+   path("request/status/set/cancel/<int:id>", CancelFriendRequest, name="cancelFirendRequest"),
    path("block/<str:_login>", BlockUser ,name="BlockUser"),
    path("unblock/<str:_login>", UnBlockUser, name='UnBlockUser'),
    path("unfriend/<str:_login>", UnFriendUser ,name="Unfriend"),
--- frontend/src/components/connectButton.tsx
+++ frontend/src/components/connectButton.tsx
@@ -60,6 +60,8 @@ import { useParams } from "react-router-dom";
 import { UserContext } from "./UserContext";
 import mailman from '@/utils/AxiosFetcher';
 import { WebSocketContext } from '@/utils/WSContext';
+import { Loading__ } from "@/auth/Login";
+
 
 const itemVariants: Variants = {
      open: {
@@ -78,10 +80,12 @@ const itemVariants: Variants = {
 function ConnectButton() {
   const [isOpen, setIsOpen] = useState(false);
   const [status, setStatus] = useState("accept");
-  const [isblock, setIsbLock] = useState("");
+  const [isblock, setIsbLock] = useState<boolean>(false);
+  const [btn_block, setBtn_block] = useState("Block");
   const [isfriend, setIsfriend] = useState("");
   const [FriendRequest, setFriendRequest] = useState("")
   const [friend_req_id, setFriendRequestId] = useState<number>(-1)
+  const [loading, setLoading] = useState<boolean>(true)
 
   const {username} = useParams();
   const userContextConsumer = useContext(UserContext)
@@ -96,18 +100,18 @@ function ConnectButton() {
     setStatus("accepted");
   };
   
-  const BlockStatusCheck = async ()=>
+  const BlockActionCheck = async ()=>
   {
       try{
-          const req = {
-              url:'friendship/is/BLOCKED/'+ username,
-              method: 'GET',
-              withCredentials:true,
-          }
-          const resp = await mailman(req)
-          const  responce:boolean = resp.data['status']
-          setIsbLock(responce ? 'UnBlock' : 'Block');
-          // console.log(resp)
+        const req = {
+          url: "friendship/"+ (isblock ? "unblock":"block")+ "/"+username,
+          method: "GET",
+          withCredentials: true,
+        }
+        const resp = await mailman(req)
+        console.log(resp)
+        setBtn_block(isblock ? 'Block' : 'Unblock');
+        setIsbLock(!isblock)
       }
       catch(err)
       {
@@ -115,40 +119,61 @@ function ConnectButton() {
       }
   }
 
-  const FriendStatusCheck = async ()=>
+  const BlockStatusCheck = async ()=>
   {
       try{
-          const req = {
-              url:'friendship/is/FRIEND/'+ username,
-              method: 'GET',
-              withCredentials:true,
-          }
-          const resp = await mailman(req)
-          const  responce:boolean = resp.data['status']
-          setIsfriend(responce ? 'UNFRIEND' : 'CONNECT' );
-          console.log(resp)
-          // if  (responce)
-          // {
-          const req2 = {
-            url:'friendship/status/'+ username,
-            method: 'GET',
-            withCredentials:true,
-          }
-          const resp2 = await mailman(req2)
-          const  st:string = resp2.data['status']
-          const  sender:boolean = resp2.data['sender']
-          const  fr_id:number = resp2.data["friend_req_id"]
-          setFriendRequestId(fr_id)
-          console.log(resp2)
-          if (st === "pending")
-            setFriendRequest((sender) ? 'Pending': 'Accept')
-          // }
-          // console.log(responce)
+        const req = {
+          url:'friendship/is/BLOCKED/'+ username,
+          method: 'GET',
+          withCredentials:true,
+        }
+        const resp = await mailman(req)
+        const  responce:boolean = resp.data['status']
+        setIsbLock(responce)
+        setBtn_block(responce ? 'UnBlock' : 'Block');
+        // console.log(resp)
       }
       catch(err)
       {
-          console.log("Friend status ", err)
+          console.log("Block status ", err)
       }
+  }
+
+
+  const FriendStatusCheck = async ()=>
+  {
+    try{
+        const req = {
+          url:'friendship/is/FRIEND/'+ username,
+          method: 'GET',
+          withCredentials:true,
+        }
+        const resp = await mailman(req)
+        const  responce:boolean = resp.data['status']
+        setIsfriend(responce ? 'UNFRIEND' : 'CONNECT' );
+        console.log(resp)
+        // if  (responce)
+        // {
+        const req2 = {
+          url:'friendship/status/'+ username,
+          method: 'GET',
+          withCredentials:true,
+        }
+        const resp2 = await mailman(req2)
+        const  st:string = resp2.data['status']
+        const  sender:boolean = resp2.data['sender']
+        const  fr_id:number = resp2.data["friend_req_id"]
+        setFriendRequestId(fr_id)
+        console.log(resp2)
+        if (st === "pending")
+          setFriendRequest((sender) ? 'Pending': 'Accept')
+        // }
+        // console.log(responce)
+    }
+    catch(err)
+    {
+        console.log("Friend status ", err)
+    }
   }
   useEffect(()=>{
     // and does the main user have block relationship with the searched one (the one in useparam)
@@ -157,7 +182,8 @@ function ConnectButton() {
     FriendStatusCheck()
     // does we have sent or recieved request, [case 1, case 2]
     // friendship ?
-    // does we have 
+    // does we have
+    setLoading(false)
   } , [username]);
   // FIRST FETCH DATA ABOUT THE USER
   // CASE 1: NO FRIEND REQUEST STATUS : SEND,BLOCK
@@ -167,8 +193,8 @@ function ConnectButton() {
 
   const send_friend_request = ()=>{
     const notification = {
-        type: 'NOTIFICATION_ADD_FRIEND',
-        to : username
+      type: 'NOTIFICATION_ADD_FRIEND',
+      to : username
     }
     const message = JSON.stringify(notification)
     SocketContext?.socket?.current?.send(message)
@@ -184,8 +210,8 @@ function ConnectButton() {
           if (friend_req_id != -1)
           {
             const req = {
-                url: `/friendship/request/status/set/accept/${friend_req_id}`,
-                method: 'GET',
+              url: `/friendship/request/status/set/accept/${friend_req_id}`,
+              method: 'GET',
             }
             const resp = await mailman(req)
             console.log(resp.data)
@@ -199,92 +225,153 @@ function ConnectButton() {
       }
   }
 
+  const Cancel_friend_request = async () =>
+  {
+      try{
+
+          if (friend_req_id != -1)
+          {
+            const req = {
+              url: `/friendship/request/status/set/cancel/${friend_req_id}`,
+              method: 'GET',
+            }
+            const resp = await mailman(req)
+            console.log(resp.data)
+            setFriendRequest("")
+            setIsfriend("CONNECT")
+          }
+          // we need to rest all thing to get back to what it should be
+      }
+      catch(e){
+          console.log(e)
+      }
+  }
+
+  const unfriend_request = async () =>
+  {
+    try{
+
+        if (friend_req_id != -1)
+        {
+          const req = {
+            url: "friendship/unfriend/"+username,
+            method: "GET",
+            withCredentials: true,
+          }
+          const resp = await mailman(req)
+          console.log(resp.data)
+          setFriendRequest("")
+          setIsfriend("CONNECT")
+        }
+        // we need to rest all thing to get back to what it should be
+    }
+    catch(e){
+        console.log(e)
+    }
+  }
+
+  
   return (
-    <motion.nav
-      initial={false}
-      animate={isOpen ? "open" : "closed"}
-      className=""
-    >
-        {/* handle normal cases */}
-        {/* <motion.button
-          className="text-white m-3 px-9 py-2 xl:h-12 xl:px-14 2xl:py-1 font-semibold rounded-2xl bg-[#5E97A9]"
-          whileTap={{ scale: 0.97 }}
+    <>
+      {
+        loading ? 
+          <Loading__/>
+        : 
+          <motion.nav
+            initial={false}
+            animate={isOpen ? "open" : "closed"}
+            className=""
+          >
+              {/* handle normal cases */}
+              {/* <motion.button
+                className="text-white m-3 px-9 py-2 xl:h-12 xl:px-14 2xl:py-1 font-semibold rounded-2xl bg-[#5E97A9]"
+                whileTap={{ scale: 0.97 }}
 
-          onClick={handleAcceptClick}
-        >
-          {status === "None" ? "Send" : (status === "Pending" ? "Pending": "Unfriend")} */}
-        {/* </motion.button> */}
-        {/* handle normal cases */}
-        { isfriend === "UNFRIEND" &&
-          <>
-            <motion.button
-              className="text-white m-3 px-9 py-2 xl:h-12 xl:px-14 2xl:py-1 font-semibold rounded-2xl bg-[#5E97A9]"
-              whileTap={{ scale: 0.97 }}
+                onClick={handleAcceptClick}
+              >
+                {status === "None" ? "Send" : (status === "Pending" ? "Pending": "Unfriend")} */}
+              {/* </motion.button> */}
+              {/* handle normal cases */}
+              { isfriend === "UNFRIEND" &&
+                <>
+                  <motion.button
+                    className="text-white m-3 px-9 py-2 xl:h-12 xl:px-14 2xl:py-1 font-semibold rounded-2xl bg-[#5E97A9]"
+                    whileTap={{ scale: 0.97 }}
 
-              onClick={handleAcceptClick}
-            >
-              UnFriend
-            </motion.button>
-            <motion.button
-              className="text-white m-3 px-9 py-2 xl:h-12 xl:px-14 2xl:py-1 font-semibold rounded-2xl bg-[#5E97A9]"
-              whileTap={{ scale: 0.97 }}
+                    onClick={unfriend_request}
+                  >
+                    UnFriend
+                  </motion.button>
+                  <motion.button
+                    className="text-white m-3 px-9 py-2 xl:h-12 xl:px-14 2xl:py-1 font-semibold rounded-2xl bg-[#5E97A9]"
+                    whileTap={{ scale: 0.97 }}
 
-              onClick={handleAcceptClick}
-            >
-              Message
-            </motion.button>
-          </>
+                    onClick={handleAcceptClick}
+                  >
+                    Message
+                  </motion.button>
+                </>
+              }
+              { isfriend === "CONNECT" && FriendRequest === "" &&
+                <>
+                  <motion.button
+                    className="text-white m-3 px-9 py-2 xl:h-12 xl:px-14 2xl:py-1 font-semibold rounded-2xl bg-[#5E97A9]"
+                    whileTap={{ scale: 0.97 }}
+
+                    onClick={send_friend_request}
+                  >
+                    Connect
+                  </motion.button>
+                </>
+              }
+              {status !== "UNFRIEND" && FriendRequest !== "" &&
+                <>
+                  <motion.button
+                    className="text-white m-3 px-9 py-2 xl:h-12 xl:px-14 2xl:py-1 font-semibold rounded-2xl bg-[#5E97A9]"
+                    whileTap={{ scale: 0.97 }}
+
+                    onClick={()=>{(FriendRequest == "Accept" && accept_friend_request())}}
+                  >
+                    {FriendRequest}
+                  </motion.button>
+                  <motion.button
+                    className="text-white m-3 px-9 py-2 xl:h-12 xl:px-14 2xl:py-1 font-semibold rounded-2xl bg-[#5E97A9]"
+                    whileTap={{ scale: 0.97 }}
+
+                    onClick={()=>{Cancel_friend_request()}}
+                  >
+                    Cancel
+                  </motion.button>
+                </>
+              }
+              {/* <motion.div
+                className=""
+                variants={{
+                  open: {
+                    clipPath: "inset(0% 0% 0% 0% round 10px)",
+                    transition: {
+                      type: "spring",
+                      bounce: 0,
+                      duration: 0.7,
+                      delayChildren: 0.3,
+                      staggerChildren: 0.05
+                        }
+                      },          closed: { clipPath: "inset(10% 50% 90% 50% round 10px)", transition: { duration: 0.3 } }
+                }}
+                style={{ pointerEvents: isOpen ? "auto" : "none" }}
+              > */}
+              <motion.button
+                className="text-white m-3 px-9 py-2 xl:h-12 xl:px-14 2xl:py-1 font-semibold rounded-2xl bg-[#5E97A9]"
+                whileTap={{ scale: 0.97 }}
+
+                onClick={BlockActionCheck}
+              >
+               {btn_block}
+              </motion.button>
+              {/* </motion.div> */}
+          </motion.nav>
         }
-        { isfriend === "CONNECT" && FriendRequest === "" &&
-          <>
-            <motion.button
-              className="text-white m-3 px-9 py-2 xl:h-12 xl:px-14 2xl:py-1 font-semibold rounded-2xl bg-[#5E97A9]"
-              whileTap={{ scale: 0.97 }}
-
-              onClick={send_friend_request}
-            >
-              Connect
-            </motion.button>
-          </>
-        }
-        {status !== "UNFRIEND" && FriendRequest !== "" &&
-          <>
-            <motion.button
-              className="text-white m-3 px-9 py-2 xl:h-12 xl:px-14 2xl:py-1 font-semibold rounded-2xl bg-[#5E97A9]"
-              whileTap={{ scale: 0.97 }}
-
-              onClick={()=>{(FriendRequest == "Accept" && accept_friend_request())}}
-            >
-              {FriendRequest}
-            </motion.button>
-          </>
-        }
-        {/* <motion.div
-          className=""
-          variants={{
-            open: {
-              clipPath: "inset(0% 0% 0% 0% round 10px)",
-              transition: {
-                type: "spring",
-                bounce: 0,
-                duration: 0.7,
-                delayChildren: 0.3,
-                staggerChildren: 0.05
-                  }
-                },          closed: { clipPath: "inset(10% 50% 90% 50% round 10px)", transition: { duration: 0.3 } }
-          }}
-          style={{ pointerEvents: isOpen ? "auto" : "none" }}
-        > */}
-        <motion.button
-          className="text-white m-3 px-9 py-2 xl:h-12 xl:px-14 2xl:py-1 font-semibold rounded-2xl bg-[#5E97A9]"
-          whileTap={{ scale: 0.97 }}
-
-          onClick={handleAcceptClick}
-        >
-          {isblock === "Block" ? "Block" : "Unblock"}
-        </motion.button>
-        {/* </motion.div> */}
-    </motion.nav>
+      </>
   );
 }
 
