7500d6b1c97c2de4ad016acd3ce585370f38f6ef
Hamza Chahboune <hachahbo@e1r4p3.1337.ma>
2024-12-23 15:22:11 +0100

    ´´hi

--- frontend/src/components/MatchHistroy.tsx
+++ frontend/src/components/MatchHistroy.tsx
@@ -17,14 +17,14 @@ export function MatchHistory() {
                     <div className="p-3  w-full h-full grid grid-rows-6 ">
                       <div className="row-span-2  px-6 flex items-center justify-between w-full">
                           <div className="flex items-center justify-center flex-col gap-3">
-                              <img className="size-14 lg;size-18  xxl:size-24 rounded-full" src={`${axiosPath}${userContextConsumer.userData?.profile_pic}`} alt="user-image" />
+                              <img className="size-14 lg;size-18  xxl:size-24 rounded-full aspect-square  object-cover" src={`${axiosPath}${userContextConsumer.userData?.profile_pic}`} alt="user-image" />
                               <div className="text-base font-medium xxl:text-base ">{` ${userContextConsumer.userData?.firstName}`}</div>
                            </div>
                           <div className="">
                             <h1 className="text-xl  xxl:text-3xl" >4 - 6</h1>
                           </div>
                           <div className="flex items-center justify-center flex-col gap-3">
-                              <img className="size-14   xxl:size-24 rounded-full" src={`${axiosPath}${userContextConsumer.userData?.profile_pic}`} alt="user-image" />
+                              <img className="size-14   xxl:size-24 aspect-square rounded-full object-cover" src={`${axiosPath}${userContextConsumer.userData?.profile_pic}`} alt="user-image" />
                               <div className="text-base font-medium  xxl:text-base ">{` ${userContextConsumer.userData?.firstName}`}</div>
                            </div>
                       </div>
@@ -39,7 +39,7 @@ export function MatchHistory() {
                                       <div className="  w-full mr-4">
                                         <div className=" flex  items-center">
                                               <div className="min-w-32 w-[100%] h-full  flex items-center">
-                                                  <img className="w-11 rounded-full" src={`${axiosPath}${userContextConsumer.userData?.profile_pic}`} alt="user-image" />
+                                                  <img className="w-11 rounded-full aspect-square rounded-full object-cover" src={`${axiosPath}${userContextConsumer.userData?.profile_pic}`} alt="user-image" />
                                                       <div className="mx-3">
                                                         <h1 className="font-medium text-xs">Hamza Chahboune</h1>
                                                         <h1 className="font-normal opacity-80 text-xs text-left">@hachahbo</h1>
@@ -50,7 +50,7 @@ export function MatchHistory() {
                                         <div className="  w-full  border-t border-[#5E97A9] rounded-full my-4"></div>
                                         <div className="flex  items-center">
                                               <div className="min-w-32 w-[100%] h-full  flex items-center">
-                                                  <img className="w-11 rounded-full" src={`${axiosPath}${userContextConsumer.userData?.profile_pic}`} alt="user-image" />
+                                                  <img className="w-11 rounded-full aspect-square rounded-full object-cover" src={`${axiosPath}${userContextConsumer.userData?.profile_pic}`} alt="user-image" />
                                                       <div className="mx-3">
                                                         <h1 className="font-medium text-xs">Hamza Chahboune</h1>
                                                         <h1 className="font-normal opacity-80 text-xs text-left">@hachahbo</h1>
@@ -69,7 +69,7 @@ export function MatchHistory() {
                                       <div className="  w-full mr-4">
                                         <div className=" flex  items-center">
                                               <div className="min-w-32 w-[100%] h-full  flex items-center">
-                                                  <img className="w-11 rounded-full" src={`${axiosPath}${userContextConsumer.userData?.profile_pic}`} alt="user-image" />
+                                                  <img className="w-11 aspect-square rounded-full object-cover" src={`${axiosPath}${userContextConsumer.userData?.profile_pic}`} alt="user-image" />
                                                       <div className="mx-3">
                                                         <h1 className="font-medium text-xs">Hamza Chahboune</h1>
                                                         <h1 className="font-normal opacity-80 text-xs text-left">@hachahbo</h1>
@@ -80,7 +80,7 @@ export function MatchHistory() {
                                         <div className="  w-full  border-t border-[#5E97A9] rounded-full my-4"></div>
                                         <div className="flex  items-center">
                                               <div className="min-w-32 w-[100%] h-full  flex items-center">
-                                                  <img className="w-11 rounded-full" src={`${axiosPath}${userContextConsumer.userData?.profile_pic}`} alt="user-image" />
+                                                  <img className="w-11 aspect-square rounded-full object-cover" src={`${axiosPath}${userContextConsumer.userData?.profile_pic}`} alt="user-image" />
                                                       <div className="mx-3">
                                                         <h1 className="font-medium text-xs">Hamza Chahboune</h1>
                                                         <h1 className="font-normal opacity-80 text-xs text-left">@hachahbo</h1>
@@ -98,7 +98,7 @@ export function MatchHistory() {
                                       <div className="  w-full mr-4">
                                         <div className=" flex  items-center">
                                               <div className="min-w-32 w-[100%] h-full  flex items-center">
-                                                  <img className="w-11 rounded-full" src={`${axiosPath}${userContextConsumer.userData?.profile_pic}`} alt="user-image" />
+                                                  <img className="w-11 aspect-square rounded-full object-cover" src={`${axiosPath}${userContextConsumer.userData?.profile_pic}`} alt="user-image" />
                                                       <div className="mx-3">
                                                         <h1 className="font-medium text-xs">Hamza Chahboune</h1>
                                                         <h1 className="font-normal opacity-80 text-xs text-left">@hachahbo</h1>
@@ -109,7 +109,7 @@ export function MatchHistory() {
                                         <div className="  w-full  border-t border-[#5E97A9] rounded-full my-4"></div>
                                         <div className="flex  items-center">
                                               <div className="min-w-32 w-[100%] h-full  flex items-center">
-                                                  <img className="w-11 rounded-full" src={`${axiosPath}${userContextConsumer.userData?.profile_pic}`} alt="user-image" />
+                                                  <img className="w-11 aspect-square rounded-full object-cover" src={`${axiosPath}${userContextConsumer.userData?.profile_pic}`} alt="user-image" />
                                                       <div className="mx-3">
                                                         <h1 className="font-medium text-xs">Hamza Chahboune</h1>
                                                         <h1 className="font-normal opacity-80 text-xs text-left">@hachahbo</h1>
