c3ead5784a9537406c5b1ce4e73c35e3468c3647
Ahmed Amine Alami <hsaktiwy@gmail.com>
2024-12-26 01:48:01 +0100

    working on updating form validations

--- backend/utils/script.sh
+++ backend/utils/script.sh
@@ -1,4 +1,4 @@
-while ! ping -c 1 -W 1 postgresql &> /dev/null; do
+while ! nc -z -v -w 1 postgresql 5432 &> /dev/null; do
   echo "Waiting for postgres container..."
   sleep 2
 done
--- frontend/src/auth/Registration/RegisterInput.tsx
+++ frontend/src/auth/Registration/RegisterInput.tsx
@@ -1,6 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
+import { inputsDataInterface } from "../RegisterForm";
+import { LuEye } from "react-icons/lu";
+import { LuEyeOff } from "react-icons/lu";
 
-const FormInput = ()=>{
+interface FormInputPropInterface {
+    name: keyof inputsDataInterface,
+    type: string,
+    errorMessage:string,
+    label: string,
+    pattern: string,
+    required: boolean,
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void,
+    value: string
+}
+const FormInput = (prop: FormInputPropInterface)=>{
+    const {name, type,label, onChange,errorMessage, ...inputProps} = prop
+    const [hide, setHide] = useState<boolean>(true)
+    const [passFocus, setPassFocus] = useState<boolean>(false)
+    const [hide2, setHide2] = useState<boolean>(true)
+    const [passFocus2, setPassFocus2] = useState<boolean>(false)
+    const [focused, setFocused] = useState<boolean>(false)
 
+    const onBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+        if (e.target.name === 'password')
+            setPassFocus(false)
+        else if (e.target.name === 'password2')
+            setPassFocus2(false)
+        setFocused(true);
+    };
+    const onFocus = (e: React.FocusEvent<HTMLInputElement>) => {
+        if (e.target.name === 'password')
+            setPassFocus(true)
+        else if (e.target.name === 'password2')
+            setPassFocus2(true)
+      };
+    return(
+        <div className={`${name === 'firstName' || name === 'lastName' ? 'w-[46%]' : 'mb-6 relative'}`}>
+
+            <label htmlFor={name} className="block text-white font-bold mb-2">{label+":"}</label>
+            <input
+                type={name !== 'password' && name !== 'password2' ? type : ((name === 'password' && hide) || (name === 'password2' && hide2)) ? type : 'text'}
+                {...inputProps}
+                autoComplete='off'
+                onChange={onChange}
+                onFocus={onFocus}
+                onBlur={onBlur}
+                
+                className={`${focused && 'focused'} bg-slate-900 w-full px-3 py-2 text-white outline-none rounded-2xl  duration-75 border border-slate-200 focus:border-slate-900 focus:bg-slate-200 focus:text-black`}
+                />
+            {
+                name === 'password' && 
+                <div className={`p-4 absolute right-1 -translate-y-[50%] top-[70%] cursor-pointer duration-75 ${passFocus ? 'text-black' : 'text-white' }`} onClick={() =>{
+                    setHide(!hide)
+                }}>
+                    {hide ? <LuEyeOff/> : <LuEye/>}
+                </div>
+            }
+            {
+                name === 'password2' &&
+                <div className={`p-4 absolute right-1 -translate-y-[50%] top-[70%] cursor-pointer duration-75 ${passFocus2 ? 'text-black' : 'text-white' }`} onClick={() =>{
+                    setHide2(!hide2)
+                }}>
+                    {hide2 ? <LuEyeOff/> : <LuEye/>}
+                </div>
+            }
+            <span>{errorMessage}</span>
+        </div>
+           
+    )
     
 }
+
+export default FormInput
