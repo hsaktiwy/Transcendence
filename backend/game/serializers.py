58bb05611ac7548a2a968bca4afb4a45d2b12ee8
hsaktiwy <hsaktiwy@gmail.com>
2025-01-18 20:11:47 +0100

    update in GameSerializer to support nested serialization applyed on user_p1 user_p2 winner loser (UserSerializer)

--- backend/game/serializers.py
+++ backend/game/serializers.py
@@ -1,7 +1,11 @@
 from rest_framework import serializers
 from .models import Game
-
+from users.serializers import UserSerializer
 class GameSerializer(serializers.ModelSerializer):
+    user_p1 = UserSerializer(read_only=True)  # Use UserSerializer for user_p1
+    user_p2 = UserSerializer(read_only=True)  # Use UserSerializer for user_p2
+    winner = UserSerializer(read_only=True)   # Use UserSerializer for winner
+    loser = UserSerializer(read_only=True)    # Use UserSerializer for loser
     class Meta:
         model = Game
         fields = '__all__'
