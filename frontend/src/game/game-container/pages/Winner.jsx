c86d23daf91dc428a37c2edfe5a8b0c93bce57df
esc-ouni <idouni@student.1337.ma>
2025-01-24 08:49:25 +0100

    INTEGRATION
    ...

--- frontend/src/game/game-container/pages/Winner.jsx
+++ frontend/src/game/game-container/pages/Winner.jsx
@@ -1,7 +1,7 @@
 
 // export default Winner;
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Winner.css";
 import PingPongBack from "../components/PingPongBack";
 import { Frame } from "../components/Frame";
@@ -10,48 +10,60 @@ import { useNavigate } from "react-router-dom";
 const Winner = () => {
   const navigate = useNavigate();
   const matchId = localStorage.getItem("matchId");
-  const matchData = JSON.parse(localStorage.getItem("Matches_data"));
-  const winner = matchData[matchId].winner;
+  const [winner, setWinner] = useState('')
+
+
+  useEffect( () => {
+      if (matchId === null){
+        navigate('/game');
+      }
+      else{
+        const matchData = JSON.parse(localStorage.getItem("Matches_data"));
+        setWinner(matchData[matchId].winner);
+      }
+    }, [matchId]
+  )
+
+
+
   return (
     <>
 
       {/* <PingPongBack /> */}
-
-      <div className="game-options-container-w">
-        <div className="game-options-header-w">
-          <h1>Winner</h1>
-        </div>
-        
-        <div className="players-container-w">
-          {/* Add your game content here */}
-          <div className="flex center-column max-width-full ">
-            <img src="bottouns/crown.png" width="50%" height="50%"/>
-            <h1 className="yellow">{winner}</h1>
+  <div className="main-game-page-container">
+        <div className="game-options-container-w">
+          <div className="game-options-header-w">
+            <h1>Winner</h1>
           </div>
-        </div>
-        <div className="button-container-w">
-          <Frame
-            text="Main Game Page"
-            default_icon='/GamePub/bottouns/default_offline.svg'
-            hovered_icon='/GamePub/bottouns/hovered_offline.svg'
-            onClick={() => {
-              localStorage.removeItem('Matches_data');
-              localStorage.removeItem('Matches_history');
-              localStorage.removeItem("matchId")
-              navigate('/game/PingPong_Lobby')
-            }}
-          />
-          <Frame
-            text="Re-Match"
-            default_icon='/GamePub/bottouns/default_offline.svg'
-            hovered_icon='/GamePub/bottouns/hovered_offline.svg'
-            onClick={() => {
-              localStorage.removeItem('Matches_data');
-              localStorage.removeItem('Matches_history');
-              localStorage.removeItem("matchId")
-              navigate('/game/LocalGame')
-            }}
-          />
+          
+          <div className="players-container-w">
+            {/* Add your game content here */}
+            <h1 >{winner}</h1>
+          </div>
+          <div className="button-container-w">
+            <Frame
+              text="Main Game Page"
+              default_icon='/GamePub/bottouns/default_offline.svg'
+              hovered_icon='/GamePub/bottouns/hovered_offline.svg'
+              onClick={() => {
+                localStorage.removeItem('Matches_data');
+                localStorage.removeItem('Matches_history');
+                localStorage.removeItem("matchId")
+                navigate('/game/PingPong_Lobby')
+              }}
+            />
+            <Frame
+              text="Re-Match"
+              default_icon='/GamePub/bottouns/default_offline.svg'
+              hovered_icon='/GamePub/bottouns/hovered_offline.svg'
+              onClick={() => {
+                localStorage.removeItem('Matches_data');
+                localStorage.removeItem('Matches_history');
+                localStorage.removeItem("matchId")
+                navigate('/game/LocalGame')
+              }}
+            />
+          </div>
         </div>
       </div>
     </>
