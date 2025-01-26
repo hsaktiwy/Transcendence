2b0a2c6693c84bd3a33429691f5ad7cf5e4de8d5
Hamza Saktiwy <hsaktiwy@e1r5p7.1337.ma>
2025-01-26 21:55:37 +0100

    fixed some bugs set some initiations

--- backend/api/consumers.py
+++ backend/api/consumers.py
@@ -59,14 +59,14 @@ class ChatConsumer(AsyncWebsocketConsumer):
             acceptedFriendReq = FriendRequest.objects.filter(sender=receiver, receiver=_sender, status="accepted").first()
             notification = Notification.objects.create(id_user_fk=receiver, content=not_content , type='friendship', friend_request_id=acceptedFriendReq.id)
             rev_notif = Notification.objects.filter(id_user_fk=_sender, friend_request_id=acceptedFriendReq.id).first()
-            channel = Channel.objects.filter(users=_sender).filter(users=receiver)
+            # channel = Channel.objects.filter(users=_sender).filter(users=receiver)
             if rev_notif:
                 rev_notif.is_readed = True
                 rev_notif.save()
-            return 1, notification, receiver.id, acceptedFriendReq.id, channel     
+            return 1, notification, receiver.id, acceptedFriendReq.id,# channel.first() if len(channel) > 0 else None
         except Exception as e:
             print(f'error  : {e}')
-            return 0, None, None, None, None
+            return 0, None, None, None,# None
     def create_message_notification(self, receiver, sender, message):
         try:
             not_content = f'{sender.login} : {message}'
@@ -178,25 +178,29 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 }
             )
     async def add_group(self, channel, user):
-        room_name = f'CHATROOM{channel.id}'
-        print(f"Adding room: {room_name}")
-        self.rooms.add(room_name)
-        print(f'Room {room_name}, added to the {user.login} goups')
+        try:
+            if channel and user:
+                room_name = f'CHATROOM{channel.id}'
+                print(f"Adding room: {room_name}")
+                self.rooms.add(room_name)
+                print(f'Room {room_name}, added to the {user.login} goups')
 
-        await self.channel_layer.group_add(
-            room_name,
-            self.channel_name
-        )
-        await self.channel_layer.group_send(
-            room_name,
-            {
-                'type': 'send_message',
-                'channel_id' : channel.id,
-                'username': user.login,
-                'ConversationType' : 'Connection',
-                'message': f'ok, I am in channel {room_name}'
-            }
-        )
+                await self.channel_layer.group_add(
+                    room_name,
+                    self.channel_name
+                )
+                await self.channel_layer.group_send(
+                    room_name,
+                    {
+                        'type': 'send_message',
+                        'channel_id' : channel.id,
+                        'username': user.login,
+                        'ConversationType' : 'Connection',
+                        'message': f'ok, I am in channel {room_name}'
+                    }
+                )
+        except Exception as e:
+            print(f"Error while trying to add to group: {e}")
 
 
 
@@ -416,8 +420,8 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 )
             elif message_json['type'] == 'NOTIFICATION_ACCEPT_FRIEND':
                 receiver = message_json['to']
-                success,notification, receiver_id, friend_request_id, channel = await sync_to_async(self.accept_friend_notification)(receiver, user)
-                await self.add_group(channel, user)
+                success,notification, receiver_id, friend_request_id = await sync_to_async(self.accept_friend_notification)(receiver, user)
+                # await self.add_group(channel, user)
                 group_name = f'notification_user_{receiver}'
                 if (success == 0):
                     return 
