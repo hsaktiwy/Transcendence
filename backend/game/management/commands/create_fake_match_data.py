46c48be52c5347286870bb2b59cb0842967c91fb
Hamza Chahboune <hachahbo@e1r6p8.1337.ma>
2025-01-27 17:42:00 +0100

    add faka data and pie chart

--- backend/game/management/commands/create_fake_match_data.py
+++ backend/game/management/commands/create_fake_match_data.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+from django.utils import timezone
 from django.core.management.base import BaseCommand
 from game.models import Game,GameType, GameEnumStatus  # Replace with your app and model name
 from users.models import MyUser  # Replace with your user model path
@@ -12,26 +14,37 @@ class Command(BaseCommand):
             self.stderr.write("Error: There should be at least two users in the database.")
             return
 
-        for _ in range(20):
-            user_p1 = random.choice(all_users)
-            user_p2 = random.choice([user for user in all_users if user != user_p1])
-            score_p1 = random.randint(0, 10)
-            score_p2 = random.randint(0, 10)
+        now = timezone.now()
+        start_date = now - timedelta(days=35)
 
-            if score_p1 > score_p2:
-                winner, loser = user_p1, user_p2
-            elif score_p2 > score_p1:
-                winner, loser = user_p2, user_p1
-            else:
-                winner, loser = None, None  # Tie game (optional logic)
+        for week in range(5):
+            week_start = start_date + timedelta(days=week * 7)
+            week_end = week_start + timedelta(days=7)
+            for _ in range(20):
+                user_p1 = random.choice(all_users)
+                user_p2 = random.choice([user for user in all_users if user != user_p1])
+                score_p1 = random.randint(0, 10)
+                score_p2 = random.randint(0, 10)
 
-            game = Game.objects.create(
-                user_p1=user_p1,
-                user_p2=user_p2,
-                winner=winner,
-                loser=loser,
-                score_p1=score_p1,
-                score_p2=score_p2,
-                status=random.choice([tag.value for tag in GameEnumStatus])
-            )
-            self.stdout.write(f"Created game: {game}")
+                time_difference = int((week_end - week_start).total_seconds())
+                random_seconds = random.randint(0, time_difference - 1)  # Fix: Avoid always hitting `week_end`
+                randtime = week_start + timedelta(seconds=random_seconds)
+
+                if score_p1 > score_p2:
+                    winner, loser = user_p1, user_p2
+                else:
+                    winner, loser = user_p2, user_p1
+
+                game = Game.objects.create(
+                    user_p1=user_p1,
+                    user_p2=user_p2,
+                    winner=winner,
+                    loser=loser,
+                    score_p1=score_p1,
+                    score_p2=score_p2,
+                    status=GameEnumStatus.ENDED,
+                )
+                game.time = randtime
+                game.save()
+                self.stdout.write(f"Created game: {game.time}")
+
--- frontend/src/components/MatchHistroy.tsx
+++ frontend/src/components/MatchHistroy.tsx
@@ -1,5 +1,5 @@
 
-import React, { useContext, useState }  from "react";
+import React, { useContext, useEffect, useState }  from "react";
 
 
 import mailman from "../utils/AxiosFetcher";
@@ -32,7 +32,7 @@ export function MatchHistory() {
     throw new Error("userContext must be used within a UserProvider");
 
     const {userMatchHistory} = userContextConsumer
-    console.log('usermatch hitrory ->>', userMatchHistory);
+    // console.log('usermatch hitrory ->>', userMatchHistory);
 
     const profiles = [
       {
@@ -78,11 +78,13 @@ export function MatchHistory() {
       
       // // Call the function
       // fetchUserMatchHistory();
-      if(userMatchHistory.length)
-          setIsPlayed(true);
+      useEffect(() => {
+        if(userMatchHistory.length)
+            setIsPlayed(true);
+      })
     return (
   <>
-       {!isPlayed ? (<div className="text-2xl  h-full w-full  rounded-2xl bg-gradient-to-br from-[#242b2f] to-[#1b1e1f]  shadow-lg  font-semibold flex flex-col justify-center items-center p-4">
+       {!isPlayed ? (<div className="text-2xl  h-full w-full  rounded-2xl   shadow-lg  font-semibold flex flex-col justify-center items-center p-4">
           <div className="flex justify-center items-center rounded-xl bg-gradient-to-bl from-[#283137] to-[#242729] flex-col p-6">
           <svg width="68" height="78" viewBox="0 0 34 39"  className="text-lg" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M10.5144 27.5068C10.7933 26.8975 11.4478 27.0026 11.4478 27.0026L11.4927 27.0077C15.354 27.8865 18.9892 27.7086 21.8527 25.6218" stroke="#FFFCFC" stroke-width="1.23" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
@@ -94,7 +96,7 @@ export function MatchHistory() {
 <path d="M27.5 30.3V32.5L28.6 33.6" stroke="white" stroke-width="0.5" stroke-linecap="round" stroke-linejoin="round"/>
 <path d="M22 32.5C22 32.6855 22.0084 32.869 22.0247 33.05M25.85 38C25.6621 37.9382 25.4786 37.866 25.3 37.7843M22.6652 35.25C22.5591 35.0456 22.4645 34.8338 22.3822 34.6154M23.5572 36.5186C23.7253 36.6997 23.9047 36.8693 24.0942 37.0261" stroke="white" stroke-width="0.5" stroke-linecap="round" stroke-linejoin="round"/>
           </svg>
-          <h1 className="text-center m-2 text-lg">there is no matches was played</h1>
+          <h1 className="text-center m-2 text-lg">You haven't played any matches yet</h1>
           </div>
        </div>)  :
               (<div className="text-2xl  h-full w-full  rounded-2xl bg-gradient-to-br from-[#242b2f] to-[#1b1e1f]  shadow-lg  font-semibold flex flex-col justify-center items-center p-4">
