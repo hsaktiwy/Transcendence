c400752cf1423d7780fc9895b637851cc2488ec6
esc-ouni <idouni@student.1337.ma>
2025-01-29 00:44:11 +0100

    REMOTE
    remote working properly

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -86,11 +86,11 @@ const App = () =>
                                 <Route path='/game/PingPong_Lobby' element={<Lobby />}/>
                                 <Route path='/game/PlayLocally_1v1' element={<PlayLocally_1v1 />}/>
                                 <Route path='/game/LocalGame' element={<LocalGame />}/>
-                                {/*<Route path='/game/PreMultiplayer' element={<PreMultiplayerGame />}/>
-                                <Route path='/game/Multiplayer' element={<MultiplayerGame />}/> */}
                                 <Route path='/game/PreRemote' element={<PreRemote />}/>
                                 <Route path='/game/RemoteGame' element={<RemoteGame />}/>
                                 <Route path='/game/Winner' element={<Winner />}/>
+                                {/*<Route path='/game/PreMultiplayer' element={<PreMultiplayerGame />}/>
+                                <Route path='/game/Multiplayer' element={<MultiplayerGame />}/> */}
                                 {/* <Route path='/game/PreTournament' element={<PreTournament />}/>
                                 <Route path='/game/Tournament' element={<Tournament />}/>
                                 <Route path='/game/Chess_Lobby' element={<ChessLobby />}/>
