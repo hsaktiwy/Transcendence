e87de8c7442887345393531da93f10223bca00b9
esc-ouni <idouni@student.1337.ma>
2025-01-29 14:39:56 +0100

    REMOTE
    States and Performance enhanced

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -82,23 +82,23 @@ const App = () =>
                               <Route path='/settings' element={ <Settings/>}/>
 
 
-                                <Route path='/game' element={<MainGamePage />}/>
-                                <Route path='/game/PingPong_Lobby' element={<Lobby />}/>
-                                <Route path='/game/PlayLocally_1v1' element={<PlayLocally_1v1 />}/>
-                                <Route path='/game/LocalGame' element={<LocalGame />}/>
-                                <Route path='/game/PreRemote' element={<PreRemote />}/>
-                                <Route path='/game/RemoteGame' element={<RemoteGame />}/>
-                                <Route path='/game/Winner' element={<Winner />}/>
-                                {/*<Route path='/game/PreMultiplayer' element={<PreMultiplayerGame />}/>
-                                <Route path='/game/Multiplayer' element={<MultiplayerGame />}/> */}
-                                {/* <Route path='/game/PreTournament' element={<PreTournament />}/>
-                                <Route path='/game/Tournament' element={<Tournament />}/>
-                                <Route path='/game/Chess_Lobby' element={<ChessLobby />}/>
-                                <Route path='/game/ChessLocally' element={<LocalChessGame />}/>
-                                <Route path='/game/ChessPreLocal' element={<ChessPreLocal />}/>
-                                <Route path='/game/ChessPreRemote' element={<ChessPreRemote />}/>
-                                <Route path='/game/ChessRemoteGame' element={<ChessRemoteGame />}/>
-                                <Route path='/game/ChessWinner' element={<ChessWinner />}/> */}
+                              <Route path='/game' element={<MainGamePage />}/>
+                              <Route path='/game/PingPong_Lobby' element={<Lobby />}/>
+                              <Route path='/game/PlayLocally_1v1' element={<PlayLocally_1v1 />}/>
+                              <Route path='/game/LocalGame' element={<LocalGame />}/>
+                              <Route path='/game/PreRemote' element={<PreRemote />}/>
+                              <Route path='/game/RemoteGame' element={<RemoteGame />}/>
+                              <Route path='/game/Winner' element={<Winner />}/>
+                              {/*<Route path='/game/PreMultiplayer' element={<PreMultiplayerGame />}/>
+                              <Route path='/game/Multiplayer' element={<MultiplayerGame />}/> */}
+                              {/* <Route path='/game/PreTournament' element={<PreTournament />}/>
+                              <Route path='/game/Tournament' element={<Tournament />}/>
+                              <Route path='/game/Chess_Lobby' element={<ChessLobby />}/>
+                              <Route path='/game/ChessLocally' element={<LocalChessGame />}/>
+                              <Route path='/game/ChessPreLocal' element={<ChessPreLocal />}/>
+                              <Route path='/game/ChessPreRemote' element={<ChessPreRemote />}/>
+                              <Route path='/game/ChessRemoteGame' element={<ChessRemoteGame />}/>
+                              <Route path='/game/ChessWinner' element={<ChessWinner />}/> */}
 
 
                             </Route>
