f2fbd26883cb895203c5bc8cc16e759ceaa6bb6c
Hamza Saktiwy <hsaktiwy@gmail.com>
2025-02-07 06:24:36 +0100

    cleaned some files only the one related to me (that i did create or a task that is totaly mine)

--- frontend/src/auth/Authication.tsx
+++ frontend/src/auth/Authication.tsx
@@ -3,45 +3,35 @@ import { Routes, Route} from 'react-router-dom'
 import { childrenInterface } from '../utils/interfaces';
 import RouteProtection from './RouteProtection';
 import Login  from './Login'
-
-
-
-
-
-
 import LandingPage2 from '@/components/Landing/LandingPage2';
-
-
 import RegisterForm from './RegisterForm';
 import { ToastContainer } from 'react-toastify';
 function Auth(children:childrenInterface)
 {
     
     return (
-                <>
-                    
-                    <ToastContainer
-                        position="top-right"
-                        autoClose={2000}
-                        hideProgressBar={false}
-                        newestOnTop={false}
-                        closeOnClick={false}
-                        rtl={false}
-                        pauseOnFocusLoss
-                        draggable
-                        pauseOnHover
-                        theme="light"
-                    />
-                    <Routes>
-                        <Route path="/*" element={<RouteProtection>{children.children}</RouteProtection>}/>
-                        <Route path="/login" element={<Login/>}/>
-                        <Route path="/signup" element={<RegisterForm/>}/>
-                        <Route path="/home" element={<LandingPage2/>}/>
-                        {/* <Route path="/site" element={<Layout><ChatSection/></Layout>}/> */}
-                    </Routes>
-                </>
-
-          
+        <>
+            
+            <ToastContainer
+                position="top-right"
+                autoClose={2000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick={false}
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
+            <Routes>
+                <Route path="/*" element={<RouteProtection>{children.children}</RouteProtection>}/>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/signup" element={<RegisterForm/>}/>
+                <Route path="/home" element={<LandingPage2/>}/>
+                {/* <Route path="/site" element={<Layout><ChatSection/></Layout>}/> */}
+            </Routes>
+        </>          
     )
 }
 
--- frontend/src/utils/ConversationsList.ts
+++ frontend/src/utils/ConversationsList.ts
@@ -1,30 +1,8 @@
 import {Conversation} from "./ChatContext"
 import mailman from "./AxiosFetcher";
-// let initila the data using the http protocol
-
 
 export const init_conv = async (setLoading:React.Dispatch<React.SetStateAction<boolean>>,setActive:React.Dispatch<React.SetStateAction<Conversation | undefined>>, setConv:React.Dispatch<React.SetStateAction<Conversation[] | undefined>> , channel_id : number | undefined) =>
 {
-  console.log("channel____id     ",channel_id)
-    let initialized:boolean = false
-    let received:boolean = false
-  console.log("bool" + initialized)
-  if (initialized)
-  {
-    if (received)
-    {
-      setLoading(false)
-     // setConv(convs)
-     // setActive(convs[0])
-    }
-      return 
-  }
-  console.log("ola ola")
-  initialized = true;
-  const data = async () =>
-  {
-    console.log("strange")
-    console.log("hmm: ", import.meta.env.VITE_MESSAGES_PACKET_SIZE)
     try {
       const url:string = import.meta.env.VITE_CONVERSATION + import.meta.env.VITE_MESSAGES_PACKET_SIZE + '/'
       const request = {
@@ -33,9 +11,7 @@ export const init_conv = async (setLoading:React.Dispatch<React.SetStateAction<b
         withCredentials: true,
       }
       const response = await mailman(request)
-      console.log(response.data)
       const  conversations:Conversation[] =  response.data.conversations as Conversation[]
-      received = true
       setLoading(false)
       setConv(conversations)
       if (channel_id !== undefined) {
@@ -48,8 +24,5 @@ export const init_conv = async (setLoading:React.Dispatch<React.SetStateAction<b
     catch (error)
     {
       console.error(error)
-      initialized = false;
     }
-  }
-  await data();
 }
