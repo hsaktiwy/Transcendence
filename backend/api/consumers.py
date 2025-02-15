8247fdfb3af663c0e835745fb82de63dd0aa1e14
Hamza Saktiwy <hsaktiwy@gmail.com>
2025-02-15 22:08:12 +0100

    added some testing and fixed one bug in conversation view

--- backend/api/consumers.py
+++ backend/api/consumers.py
@@ -511,6 +511,8 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def state(self, event):
         message = json.dumps(event)
         await self.send(text_data=message)
+
+
 class NoMatchConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         await self.accept()
