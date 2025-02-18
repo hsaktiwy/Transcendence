45e8d69e2fad754f9d9871fe817d45eecbe2add2
Hamza Saktiwy <hsaktiwy@gmail.com>
2025-02-18 23:29:22 +0100

    update on the production level

--- build.sh
+++
@@ -1,5 +0,0 @@
-# mkdir postgres_volume
-# chmod +x postgres_volume
-# docker compose up --build
-find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
-find . -path "*/migrations/*.pyc"  -delete
--- frontend/tools/script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-
-cp -rfu /cache/node_modules/. /app/node_modules/
-npm --prefix /app/ run dev
