9de49b96fefd79eb7ade9e6982bedecc405fa290
esc-ouni <idouni@student.1337.ma>
2025-02-19 11:50:21 +0100

    TBT Retouches (Text, Infos, ...)

--- frontend/src/game/game-container/pages/MainGamePage.jsx
+++ frontend/src/game/game-container/pages/MainGamePage.jsx
@@ -18,17 +18,19 @@ function MainGamePage(){
                     <div className="lktaba1">THE PLATFORM</div>
                     <div className="lktaba2">PLATFORM DESCIEPTION :</div>
                     <p className="lktaba3">
-                        Lorem Ipsum is simply dummy text of the printing
+                        {/* Lorem Ipsum is simply dummy text of the printing
                         and&nbsp;&nbsp;typesetting industry. Lorem Ipsum has been the
                         industry&#39;s standard dumm y text ever since the 1500s, when
-                        an unknown. Have Fun !
+                        an unknown. Have Fun ! */}
+                    {/* Experience the ultimate fusion of strategy and adrenaline in this all-in-one 3D gaming web app! Dive into lightning-fast Ping Pong matches where reflexes reign supreme, or flex your brainpower in visually stunning 3D Chess duels. Challenge friends locally, climb global leaderboards in competitive multiplayer, or dominate fierce tournaments—each victory forging your legacy. With seamless matchmaking, immersive visuals, and endless ways to play, this web app takes your gaming experience to thrilling new heights, Have Fun ! */}
+                    Experience the thrill of fast-paced 3D Ping Pong and the cerebral challenge of immersive 3D Chess—all in one seamless web app. Hone your reflexes in intense table tennis battles, or devise brilliant strategies in stunning board showdowns. With sleek visuals and easy connectivity, the future of online gaming awaits at your fingertips! Have fun !
                     </p>
                 </div>
 
                 <div className={"teams-container"} >
                     <div className="team" onClick={() => {navigate('/game/Chess_Lobby')}}>
                         <center>
-                            <h1>Le7ya's Game</h1>
+                            <h1>Chess Game</h1>
                         </center>
                     </div>
                     <div className="team" onClick={() => {navigate('/game/PingPong_Lobby')}}>
